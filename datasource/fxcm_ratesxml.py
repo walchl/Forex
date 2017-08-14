f55285bddd0386ab2c126316c0cb437e32bf4b73
Walter C. H. Lu (Win10@DUO13) <walter.walchl@gmail.com>
2017-08-14 08:39:59 +0800

    Change FXCM XML url from RatesXML to RatesXML3.

--- datasource/fxcm_ratesxml.py
+++ datasource/fxcm_ratesxml.py
@@ -3,14 +3,15 @@ from lxml import etree
 from datasource.types import *
 
 def Fetch_DailyPrices( CURRENCY ):
-    URL = 'http://rates.fxcm.com/RatesXML'
+    URL = 'http://rates.fxcm.com/RatesXML3'
     table = etree.parse( URL ).getroot()
 
     # Fetch All Prices
     price = {}
     for ele in table:
-        price[ele.get('Symbol')] = (float(ele.find('Bid').text) +
-                                    float(ele.find('Ask').text)) /2
+        price[ele.find('Symbol').text] = (
+            (float(ele.find('Bid').text) + float(ele.find('Ask').text)) /2,
+            ele.find('Last').text, ele.find('Date').text )
 
     # Build price_list[]
     price_list = []
@@ -24,12 +25,12 @@ def Fetch_DailyPrices( CURRENCY ):
         if symbol == PIVOT:
             pivot_price = 1.0
         elif PIVOT+symbol in price:
-            pivot_price = price[PIVOT+symbol]
+            pivot_price = price[PIVOT+symbol][0]
         elif symbol+PIVOT in price:
-            pivot_price = 1.0/price[symbol+PIVOT]
+            pivot_price = 1.0/price[symbol+PIVOT][0]
 
         price_list.append( pivot_price )
 
-    timestamp = table[0].find('Last').text
-    print( 'FXCM RatesXML at {} Fetched'.format(timestamp) )
-    return [ DailyPrices( timestamp, price_list ) ]
+    eur = price['EURUSD']
+    print( 'FXCM RatesXML at ({},{}) Fetched'.format( eur[1], eur[2] ) )
+    return [ DailyPrices( eur[1], price_list ) ]
